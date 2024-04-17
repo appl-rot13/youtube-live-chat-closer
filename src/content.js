4e828e69f9ba62b3e68aa0e9ac23eab853790741
rot <appl.rot13@gmail.com>
2024-04-17 20:27:00 +0900

    Improve to close chat when switching to enabled while chat is displayed

--- src/content.js
+++ src/content.js
@@ -1,15 +1,27 @@
-(async () => {
-  const enabled = await chrome.runtime.sendMessage("get-enabled");
-  if (!enabled) {
-    return;
+(() => {
+  const closeChat = async () => {
+    const enabled = await chrome.runtime.sendMessage("get-enabled");
+    if (!enabled) {
+      return;
+    }
+
+    const closeButton = document.querySelector("#close-button button");
+    if (!closeButton) {
+      return;
+    }
+
+    const event = document.createEvent("MouseEvents");
+    event.initEvent("click", true, true);
+    closeButton.dispatchEvent(event);
   }
 
-  const closeButton = document.querySelector("#close-button button");
-  if (!closeButton) {
-    return;
-  }
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    switch (message) {
+      case "close-chat":
+        closeChat();
+        break;
+    }
+  });
 
-  const event = document.createEvent("MouseEvents");
-  event.initEvent("click", true, true);
-  closeButton.dispatchEvent(event);
+  closeChat();
 })();
--- src/popup.js
+++ src/popup.js
@@ -5,5 +5,21 @@
   input.addEventListener("click", () => {
     const state = input.checked ? "enable" : "disable";
     chrome.runtime.sendMessage("set-to-" + state);
+
+    if (!input.checked) {
+      return;
+    }
+
+    // Close chat only for active tab
+    // chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    //   chrome.tabs.sendMessage(tabs[0].id, "close-chat").catch(error => {});
+    // });
+
+    // Close chat for all tabs
+    chrome.tabs.query({}, tabs => {
+      tabs.forEach(tab => {
+        chrome.tabs.sendMessage(tab.id, "close-chat").catch(error => {});
+      });
+    });
   });
 })();
