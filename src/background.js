23d825825fb3a7cada62259a05b5588f916b1bfa
rot <appl.rot13@gmail.com>
2024-04-17 16:24:52 +0900

    Change to save enable/disable state in local storage

--- src/background.js
+++ src/background.js
@@ -1,19 +1,23 @@
 (() => {
-  let enabled = true;
+  const isEnabled = "isEnabled";
 
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     switch (message) {
       case "set-to-enable":
-        enabled = true;
+        chrome.storage.local.set({ [isEnabled]: true });
         break;
 
       case "set-to-disable":
-        enabled = false;
+        chrome.storage.local.set({ [isEnabled]: false });
         break;
 
       case "get-enabled":
-        sendResponse(enabled);
+        chrome.storage.local.get([isEnabled]).then(ret => {
+          sendResponse((isEnabled in ret) ? ret[isEnabled] : true);
+        });
         break;
     }
+
+    return true;
   });
 })();
